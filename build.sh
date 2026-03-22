d85de2ea02b990dd0ef5d235da17fa2c5b453968
Sagar Patil <sagarreddypatil@gmail.com>
2026-03-22 02:36:02 -0700

    change pfx

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+PREFIX=/opt/wine-gl46
 export PATH="/usr/local/opt/bison/bin:/usr/local/bin:$PATH"
-export CPPFLAGS="-I$HOME/mesa-native/include"
-export LDFLAGS="-L/usr/local/lib -L$HOME/mesa-native/lib -Wl,-rpath,/usr/local/lib -Wl,-rpath,$HOME/mesa-native/lib"
-export PKG_CONFIG_PATH="$HOME/mesa-native/lib/pkgconfig"
-rm -f config.cache
+export CPPFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L/usr/local/lib -L$PREFIX/lib -Wl,-rpath,/usr/local/lib -Wl,-rpath,$PREFIX/lib"
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+# rm -f config.cache
 
-arch -x86_64 make distclean || true
+# arch -x86_64 make distclean || true
 
 arch -x86_64 ./configure \
-  --prefix="$HOME/wine-custom" \
+  --prefix="$PREFIX" \
   --enable-win64 \
   --with-opengl \
   --without-vulkan \
--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
-# export DYLD_INSERT_LIBRARIES=$HOME/mesa-native/lib/libgl_interpose.dylib
-# export DYLD_LIBRARY_PATH=$HOME/mesa-native/lib
-export LIBGL_DRIVERS_PATH=$HOME/mesa-native/lib/dri
-export VK_DRIVER_FILES=$HOME/mesa-native/share/vulkan/icd.d/kosmickrisp_mesa_icd.x86_64.json
+PREFIX=/opt/wine-gl46
+# export DYLD_INSERT_LIBRARIES=$PREFIX/lib/libgl_interpose.dylib
+# export DYLD_LIBRARY_PATH=$PREFIX/lib
+export LIBGL_DRIVERS_PATH=$PREFIX/lib/dri
+export VK_DRIVER_FILES=$PREFIX/share/vulkan/icd.d/kosmickrisp_mesa_icd.x86_64.json
 export EGL_PLATFORM=surfaceless
 export MESA_LOADER_DRIVER_OVERRIDE=zink
 export MESA_GL_VERSION_OVERRIDE=4.6
 export MESA_GLSL_VERSION_OVERRIDE=460
 
+WINE=$PREFIX/bin/wine
+
 cd ~/Downloads/
-# ~/wine-custom/bin/wine glxgears.x64.exe
-~/wine-custom/bin/wine gl46test.exe
+$WINE gl46test.exe
+$WINE glxgears.x64.exe
 # cd ~/Downloads/GLSLTestbed-Release
-# ~/wine-custom/bin/wine GLSLTestbed.exe
+# $WINE GLSLTestbed.exe
